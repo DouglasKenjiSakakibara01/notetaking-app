e9921d1d7befca452691a74bfa9a550050d1e52c
DouglasKenjiSakakibara01 <douglaskenjisakakibara@gmail.com>
2025-08-13 23:07:23 -0300

    iniciando tela de login

---
+++ src/app/paginas/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  senha = new FormControl('', [Validators.required, Validators.minLength(8)]);
+
+  Login() {
+    console.log('Login efetuado com sucesso!');
+  }
+}
