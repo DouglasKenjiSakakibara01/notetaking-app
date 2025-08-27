5a7c972c3b7ae03e326c45cc1533ba04bf55e4e4
DouglasKenjiSakakibara01 <douglaskenjisakakibara@gmail.com>
2025-08-26 23:40:26 -0300

    refatorado estrutura dos arquivos do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './paginas/login/login';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [
     {
@@ -12,10 +12,10 @@ export const routes: Routes = [
     },
     {
         path: 'cadastro-conta',
-        loadComponent: () => import('./paginas/cadastro-conta/cadastro-conta').then(m => m.CadastroConta)
+        loadComponent: () => import('./features/cadastro-conta/cadastro-conta').then(m => m.CadastroConta)
     },
     {
         path: 'home',
-        loadComponent: () => import('./paginas/home/home').then(m => m.Home)
+        loadComponent: () => import('./features/home/home').then(m => m.Home)
     }
 ];
