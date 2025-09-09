48536656173d45431ef70a7b89cf5a93fdbf7f87
DouglasKenjiSakakibara01 <douglaskenjisakakibara@gmail.com>
2025-09-08 23:20:57 -0300

    finalizado tela de evento - ainda falta ajustes

--- src/app/features/evento/pagina-evento.ts
+++ src/app/features/evento/pagina-evento.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { EventoService } from '../../core/services/eventoservice';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { DateAdapter, MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,7 +13,7 @@ import { MatIconModule } from "@angular/material/icon";
 @Component({
   selector: 'app-evento',
   imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDatepickerModule, MatProgressSpinnerModule, MatIconModule],
-  providers: [EventoService, provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }],
+  providers: [EventoService, provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }, DatePipe],
   templateUrl: './pagina-evento.html',
   styleUrl: './pagina-evento.scss'
 })
@@ -62,7 +62,21 @@ export class PaginaEvento {
   AbrirFormulario(item?: Evento, consultando: boolean = false) {
     if (item) {
       this.eventoForm = item;
-      consultando? this.editandoForm = true : this.consultandoForm = true;
+      this.formCadastro.patchValue({
+        titulo: this.eventoForm.Titulo,
+        descricao: this.eventoForm.Descricao,
+        dtEvento: this.eventoForm.DtEvento ? new Date(this.eventoForm.DtEvento) : null
+      });
+      
+      if (consultando) {
+        this.consultandoForm = true;
+        this.editandoForm = false;
+        this.formCadastro.disable();
+      }
+      else {
+        this.consultandoForm = false;
+        this.editandoForm = true;
+      }
     }
 
     this.formEvento = !this.formEvento; 
@@ -79,10 +93,13 @@ export class PaginaEvento {
 
   Cadastrar() {
     let evento = new Evento();
-    if (this.formCadastro.value.dtEvento) evento.DtEvento = new Date(this.formCadastro.value.dtEvento);
+    if (this.eventoForm?.Id) evento.Id = this.eventoForm.Id;
+    if (this.formCadastro.value.dtEvento) evento.DtEvento = this.formCadastro.value.dtEvento;
     if (this.formCadastro.value.titulo) evento.Titulo = this.formCadastro.value.titulo;
+    if (this.formCadastro.value.descricao) evento.Descricao = this.formCadastro.value.descricao;
 
     this.formEvento = !this.formEvento;
+
     this.eventoService.InsertOrUpdate(evento).subscribe({
       next: (evento) => {
         this.CarregarLista();
@@ -91,5 +108,10 @@ export class PaginaEvento {
         console.error('Erro ao inserir evento', erro);
       }
     });
+  }
+
+  VoltarListaEvento() {
+    this.formEvento = !this.formEvento;
+    this.formCadastro.reset();
   }
 }
